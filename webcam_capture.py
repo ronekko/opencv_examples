561f369bb553471dcadebdb6ddd6a6a86922708d
ronekko <ronekko@gmail.com>
2018-03-30 15:19:08 +0900

    Change the key to stop webcam

--- webcam_capture.py
+++ webcam_capture.py
@@ -10,7 +10,8 @@ import cv2
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
-    while cv2.waitKey(1) == 255:  # Press any key to stop capture.
+    # Press 'q' key to stop capture.
+    while cv2.waitKey(1) == ord('q'):
         ret, image = cap.read()
         cv2.imshow('camera', image)
 
